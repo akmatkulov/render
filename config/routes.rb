ce0ea4e48caa536733bc8c277e0f9f43c7be3d6c
sovetbek <akmatkulov21@gmail.com>
2024-09-17 23:48:42 +0600

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'static_pages/help'
-  get 'static_pages/about'
-
+  # Static Pages
   root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+
+  # Users Pages
+  get 'signup' => "users#new"
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,17 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe 'StaticPages', type: :request do
+
+  describe 'layout links' do
+    it 'links' do
+      get root_path
+      assert_select "a", text: "About", count: 2
+      assert_select "a", text: "Contact", count: 1
+      assert_select "a", text: "News", count: 1
+    end
+  end
+
   describe 'GET /help' do
     it 'renders a successful response' do
-      get '/static_pages/help'
+      get help_path
       assert_select 'title', 'Help | Sample App'
       expect(response).to be_successful
     end
   end
 
+  describe 'GET /contact' do
+    it 'renders a successful response' do
+      get contact_path
+      assert_select 'title', 'Contact | Sample App'
+      expect(response).to be_successful
+    end
+  end
   describe 'GET /about' do
     it 'renders a successful response' do
-      get '/static_pages/about'
+      get about_path
       assert_select 'title', 'About | Sample App'
       expect(response).to be_successful
     end
